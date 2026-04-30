98c98cd7abe3550e7155dcb31fc3006fe5169774
Alexmayhua <alexmayhuaquispe@gmail.com>
2026-04-30 16:40:59 -0500

    Add TypeScript interfaces for NavItem, Product, Collection, and Feature

---
+++ types/index.ts
@@ -0,0 +1,22 @@
+export interface NavItem {
+  label: string;
+  href: string;
+}
+
+export interface Product {
+  title: string;
+  description: string;
+  icon?: string;
+}
+
+export interface Collection {
+  title: string;
+  description: string;
+  tag: string;
+}
+
+export interface Feature {
+  title: string;
+  description: string;
+  icon?: string;
+}
